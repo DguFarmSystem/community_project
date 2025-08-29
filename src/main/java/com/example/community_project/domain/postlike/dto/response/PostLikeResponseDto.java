57d2f562cb086860a59f8b999b092b0bc0c2ab4f
WOOHYUK <wh150800@gmail.com>
2025-08-29 14:35:53 +0900

    [feat] 좋아요 기능 구현

---
+++ src/main/java/com/example/community_project/domain/postlike/dto/response/PostLikeResponseDto.java
@@ -0,0 +1,39 @@
+package com.example.community_project.domain.postlike.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class PostLikeResponseDto {
+
+    @Getter
+    @NoArgsConstructor
+    @RequiredArgsConstructor
+    public static class Data {
+        private boolean liked;
+        private long likeCount;
+
+        @Builder
+        public Data(boolean liked, long likeCount) {
+            this.liked = liked;
+            this.likeCount = likeCount;
+        }
+    }
+
+    private int status;
+    private boolean success;
+    private String message;
+    private Data data;
+
+    @Builder
+    public PostLikeResponseDto(int status, boolean success, String message, Data data) {
+        this.status = status;
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/example/community_project/domain/postlike/entity/PostLike.java
@@ -0,0 +1,46 @@
+package com.example.community_project.domain.postlike.entity;
+
+import com.example.community_project.domain.auth.entity.User;
+import com.example.community_project.domain.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Table(name="post_likes", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_post_likes",
+                columnNames = {"user_id", "post_id"}
+        )
+})
+public class PostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public PostLike(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
+
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
